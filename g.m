de94c9726383b9c3cf0bfe41c655fd6d02e1b2af
chris0731 <ksh.jin@gmail.com>
2019-03-30 19:18:43 -0400

    Jacobian

--- g.m
+++ g.m
@@ -1,5 +1,12 @@
-function matrix_g = g(phi, theta, psi)
+function matrix_g = g(q)
 %this is defined to get the matrix G(q) for Z-X-Y Euler Transformation%
+
+phi = q(1);
+theta = q(2);
+psi = q(3);
+
+
 matrix_g = [cos(theta) 0 -(cos(phi)*sin(theta)) ; 0 1 sin(phi) ; sin(theta) 0 (cos(phi)*cos(theta))];
+
 end
 
--- r.m
+++ r.m
@@ -1,4 +1,13 @@
-function matrix_r = r(phi, theta, psi)
+function matrix_r = r(q)
+   
+%to get the rotation matrix for Z-X-Y euler transformation
+
+phi = q(1);
+theta = q(2);
+psi = q(3);
+
+
+
 comp_1 = (cos(psi)*cos(theta)) - (sin(phi)*sin(psi)*sin(theta));
 comp_2 = -(cos(phi)*cos(psi));
 comp_3 = (cos(psi)*sin(theta)) + (cos(theta)*sin(phi)*sin(psi));
@@ -8,6 +17,8 @@ comp_6 = (sin(psi)*sin(theta)) - (cos(psi)*cos(theta)*sin(phi));
 comp_7 = -(cos(phi)*sin(theta));
 comp_8 = sin(phi);
 comp_9 = cos(phi)*cos(theta);
-matrix_r = [comp_1 comp_2 comp_3 ; comp_4 comp_5 comp_6 ; comp_7 comp_8 comp_9];
-end
 
+matrix_r = [comp_1 comp_2 comp_3 ; comp_4 comp_5 comp_6 ; comp_7 comp_8 comp_9];
+
+
+end
