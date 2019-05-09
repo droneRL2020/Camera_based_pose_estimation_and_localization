f9f835dc24031698fc9db5c85c2a4f4567fb5ea9
Killermachine <varshneytanay@gmail.com>
2019-05-09 18:39:50 -0400

    Minor fix in calc velo

--- calculate_velo.m
+++ calculate_velo.m
@@ -1,2 +1,4 @@
 function [velocity_optical,omega_optical] = calculate_velo(data,vicon,time_vicon,time_data,omg_imu,acc_imu,imu_data_ctr,vicon_ctr)
+velocity_optical = [0 0 0];
+omega_optical = [0 0 0];
 end
